92b8e27887d7af0c7e7820d233ce734b714ce540
Stanimir Stoychev <xstukix@gmail.com>
2022-11-04 19:12:07 +0100

    Cleanup

--- types/node.d.ts
+++ types/node.d.ts
@@ -1,7 +1,5 @@
 declare namespace NodeJS {
     export interface ProcessEnv {
-        NEXT_AUTH_GITHUB_ID: string;
-        NEXT_AUTH_GITHUB_SECRET: string;
         DATABASE_URL: string;
     }
 }
